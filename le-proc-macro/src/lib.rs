6e1af2b3d0ffedad04ebeb9d4d5a58dd3b076568
Pascal Hertleif <pascal@technocreatives.com>
2019-06-26 11:14:54 +0200

    Adding nom breaks it

--- le-proc-macro/src/lib.rs
+++ le-proc-macro/src/lib.rs
@@ -1,6 +1,8 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
 
+use nom::IResult;
+
 #[proc_macro_derive(AnswerFn)]
 pub fn derive_answer_fn(_item: TokenStream) -> TokenStream {
     "fn answer() -> u32 { 42 }".parse().unwrap()
